683064cb7bfa9d1f2fa1d8676a3178e3284c5348
Acássio Monteiro <montteiro.dsgn@gmail.com>
2023-04-24 11:57:12 -0300

    Initial commit

---
+++ comfuturo/src/App.jsx
@@ -0,0 +1,17 @@
+// react, componentes, estáticos
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1> Com futuro</h1>
+      <home />
+
+    </div>
+
+  )
+}
+
+export default App
---
+++ comfuturo/src/components/home.jsx
@@ -0,0 +1,14 @@
+import img from '../img';
+import "./home.css";
+
+export const home = () => {
+  return (
+    <div id="home">
+        <h2> Qual carreira dá<br> match comigo?</br></h2>
+        <p>Junte-se a mais de 1000 estudantes e descubra qual carreira combina com você!</p>
+        <button>Teste agora</button>
+        <img src="" alt=""/>
+
+    </div>
+  )
+}

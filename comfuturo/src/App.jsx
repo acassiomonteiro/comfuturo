b1359178aa460a115123ce400c16c3679401a10a
Acássio Monteiro <montteiro.dsgn@gmail.com>
2023-05-22 16:56:49 -0300

    Intregação com PHP
    conectando DB

--- comfuturo/src/App.jsx
+++ comfuturo/src/App.jsx
@@ -5,18 +5,20 @@ import Cursos from './pages/Cursos'
 import Sobre from './pages/Sobre'
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro'
+import Linguistica from './pages/Linguistica'
 
 function App() {
   return (
     // Rotas
     <>
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/cursos" element={<Cursos/>}/>
         <Route path="/sobre" element={<Sobre/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/cadastro" element={<Cadastro/>}/>
+        <Route path="/linguistica" element={<Linguistica/>}/>
       </Routes>
     </>
   )
---
+++ comfuturo/src/pages/Linguistica.jsx
@@ -0,0 +1,45 @@
+import Bannerp from '../img/bannerdecole.png'
+
+function Linguistica() {
+  return (
+<section class="content-center">
+        <div className="flex items-center justify-center px-4 py-20">
+           <img
+            className="flex items-center justify-center"
+            src={Bannerp}
+            alt=""
+            />
+        </div>
+        
+        <div className=' flex flex-nowrap gap-5'>
+        <div>
+            <p className='text-2xl'> Sobre o curso de Linguística</p>
+        </div>
+        <div>
+             <p className='text-base'>O curso de Linguística forma profissionais que estudam e refletem criticamente sobre o funcionamento da língua e da linguagem. Por conta disso, as disciplinas presentes no curso permitem que o aluno compreenda os processos linguísticos, desde a sua fonética até a formação das palavras, gramática e toda forma de linguagem verbal.
+                Ao final da graduação, o linguista estará apto a elaborar materiais de instrução, analisar o funcionamento linguístico-discursivo de novas tecnologias de comunicação e compreender a relação histórica das sociedades e idiomas.
+                O profissional poderá analisar e elaborar dicionários, textos de diversos gêneros para circulação em diferentes meios. Também terá a opção de desenvolver atividades nas áreas de docência e pesquisa, treinamento em línguas estrangeiras, entre outras.
+                O curso de Linguística pode ser encontrado em nível de graduação, com duração média de quatro anos, e
+                pós-graduação, com duração média de seis meses a dois anos. Além disso, o curso poderá ser realizado no formato presencial ou EaD.
+                O tipo de formação para o curso de Linguística é: pós-graduação.</p>
+        </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</section>
+
+  )
+}
+
+export default Linguistica

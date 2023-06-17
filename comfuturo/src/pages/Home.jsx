f9c5e456f5477f7087775149fb19649cb1f26cbd
fernanndogg <105644403+fernanndogg@users.noreply.github.com>
2023-06-16 23:43:09 -0300

    continuação

--- comfuturo/src/pages/Home.jsx
+++ comfuturo/src/pages/Home.jsx
@@ -5,10 +5,11 @@ import Bg3 from "../img/perfil.svg";
 import Bg4 from "../img/profissão.svg";
 import Bg5 from "../img/10min.svg";
 import Bg6 from "../img/analise-completa.svg";
+import Bg7 from "../img/image-10.svg";
 
 function Home() { 
   return (
-    <div class="bg-white">
+    <div className="bg-white">
     <section class="bg-[#FCF8F1] bg-opacity-30 py-10 sm:py-16 lg:py-24">
         <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
@@ -132,23 +133,42 @@ A partir da experiência do Com Futuro em ajudar os estudantes a se prepararem p
         </div>
     </div>
 
-    <div class="grid grid-rows-3 grid-flow-col gap -mt-96">
-        <div class="row-span-3 ...">
+    <div className="grid grid-rows-3 grid-flow-col gap -mt-96">
+        <div className="row-span-3 ...">
            <img src={Bg6} className="ml-56 scale-75 -mt-96" alt="" />
         </div>
-        <div class="col-span-2 ... -mt-96 -ml-96">
-            <h2 className="relative font-bold left-96 mt-16">UMA ANÁLISE COMPLETA DO SEU PERFIL</h2>
-            <p className="relative font-medium max-w-sm ml-96 mt-8">
+        <div className="col-span-2 ... -mt-96 -ml-96">
+            <h2 className="relative font-bold text-xl ml-80 mt-16">UMA ANÁLISE COMPLETA DO SEU PERFIL</h2>
+            <p className="relative font-medium max-w-sm text-lg ml-80 mt-8">
 A partir de uma conceituada metodologia, chamada DISC, utilizada por empresas no processo de contratação de seus funcionários, você entenderá melhor a seu respeito, seus pontos fortes e quais áreas precisam ser aprimoradas para que você alcance sucesso
 na sua vida pessoal e profissional.</p>
         </div>
-        <div class="row-span-2 col-span-2 ... -ml-50">
-        <h2 className="relative font-bold left-90 -mt-44 text-violet-500">Nessa etapa você vai descobrir:</h2>
+        <div className="row-span-2 col-span-2 .. -ml-16">
+        <h2 className="relative font-bold text-lg left-90 -mt-24 text-violet-500">Nessa etapa você vai descobrir:</h2>
         
-        <p className="flex items-center gap-3 text font-semibold left-90 mt-4"><CheckCircle/>Quais são as profissões mais indicadas para o seu perfil</p>
-        <p className="flex items-center gap-3 text font-semibold left-90 mt-4"><CheckCircle/>Qual o salário médio de cada uma dessas profissões</p>
-
+        <p className="flex items-center gap-3 text-lg font-semibold left-90 mt-4 "><CheckCircle className="text-violet-500"/>Qual é o seu estilo comportamental <br/>e o que isso significa de forma prática</p>
+        <p className="flex items-center gap-3 text-lg font-semibold left-90 mt-4 "><CheckCircle className="text-violet-500"/>Quais são seus pontos fortes a serem<br/>
+potencializados e pontos fracos a<br/>
+serem minimizados ou melhorados</p>
+        <p className="flex items-center gap-3 text-lg font-semibold left-90 mt-4"><CheckCircle className="text-violet-500"/>Suas principais motivações e seus medos</p>
         </div>
+    </div>
+
+    <div className="grid grid-rows-3 grid-flow-col gap-4">
+        <div className="row-span-3 ...">
+            <img src={Bg7} className="ml-80 mt-44 scale-100" alt="" />
+        </div>
+        <div className="col-span-2 ... mt-28">
+        <h2 className="font-bold text-4xl left-96 mt-16 -ml-20">É importante lembrar</h2>
+            <p className=" text-xl text-justify mt-10 -ml-20">O resultado apresentado no teste vocacional da<br/> com futuro não é definitivo e não deve ser a única<br/> base para escolher sua carreira. Utilize-o para<br/> descobrir novas possibilidades ou para reforçar<br/> suas ideias, mas sempre priorize seus gostos,<br/> vontades e aspirações na hora de definir sua<br/> profissão!<br/> Apesar de ter sido desenvolvido com apoio de<br/> uma equipe de psicólogos, o teste não pode ser<br/> comercializado e não passou por avaliação do<br/> Conselho Federal de Psicologia. Não é um teste<br/> para uso de avaliação psicológica e sim um teste<br/> online criado com método de associação de<br/> padrão de respostas.</p>
+        </div>
+       
+    </div>
+
+    <div className="flex flex-col ... bg-violet-500">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
     </div>
 
 </div>

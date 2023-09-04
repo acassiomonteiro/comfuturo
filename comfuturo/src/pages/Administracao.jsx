fed99f3aa6ec72d3790c8a8a3b603fa99e4ee244
fernanndogg <105644403+fernanndogg@users.noreply.github.com>
2023-09-04 12:02:40 -0300

    uma pagina adicionada

---
+++ comfuturo/src/pages/Administracao.jsx
@@ -0,0 +1,137 @@
+function Administracao() {
+    return (
+      <section class="">
+<section class="py-10 bg-gray-50 sm:py-2 lg:py-10">
+    <div class="max-w-7xl px-5 mx-auto sm:px-6 lg:px-8">
+        <div class="grid items-center grid-cols-1 lg:items-stretch md:grid-cols-2 gap-y-8 gap-x-12 xl:gap-x-20">
+            <div class="flex flex-col justify-between md:py-5">
+                <blockquote>
+                    <p class="text-4xl font-bold leading-relaxed text-black">Administração</p>
+                    <p class="text-xl font-normal text-black">Saiba tudo sobre o curso de Administração e comece a estudar com ajuda da Quero Bolsa</p>
+                </blockquote>
+
+
+            </div>
+            <div class="relative">
+                <div class="aspect-w-4 aspect-h-3">
+                    <img class="object-cover w-full h-full" src="https://kong.quero.com/il03oc02ld_001.svg" alt="" className="scale-75 -mt-12"/>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section>
+  
+    <div class=" border-b border-gray-200">
+        <div class="px-4 mx-auto sm:px-32 lg:px-36">
+            <nav class="relative flex items-center justify-between h-16 lg:h-20">
+                <div class="hidden lg:flex lg:items-center lg:space-x-10">
+                    <a href="#" title="" class="text-lg font-medium text-violet-500 hover:bg-violet-200 rounded-md px-3 py-2"> Sobre o curso </a>
+
+                    <a href="#" title="" class="text-lg font-medium text-violet-500 hover:bg-violet-200 rounded-md px-3 py-2"> Sobre a carreira </a>
+
+                    <a href="#" title="" class="text-lg font-medium text-violet-500 hover:bg-violet-200 rounded-md px-3 py-2"> Como ingressar </a>
+
+                    <a href="#" title="" class="text-lg font-medium text-violet-500 hover:bg-violet-200 rounded-md px-3 py-2"> Nota de corte </a>
+                </div>                
+            </nav>
+        </div>
+    </div>
+
+    
+    <nav class="py-4 bg-white lg:hidden ">
+        <div class="px-4 mx-auto sm:px-6 lg:px-8">
+
+            <div class="mt-6">
+                <div class="flex flex-col space-y-2">
+                    <a href="#" title="" class="py-2 text-lg font-medium text-black transition-all duration-200 focus:text-blue-600 hover:bg-violet-200 rounded-md px-3 py-2"> Sobre o curso </a>
+
+                    <a href="#" title="" class="py-2 text-lg font-medium text-black transition-all duration-200 focus:text-blue-600 hover:bg-violet-200 rounded-md px-3 py-2">  Sobre a carreira </a>
+
+                    <a href="#" title="" class="py-2 text-lg font-medium text-black transition-all duration-200 focus:text-blue-600 hover:bg-violet-200 rounded-md px-3 py-2"> Como ingressar </a>
+
+                    <a href="#" title="" class="py-2 text-lg font-medium text-black transition-all duration-200 focus:text-blue-600 hover:bg-violet-200 rounded-md px-3 py-2"> Nota de corte </a>
+                </div>
+
+                
+            </div>
+        </div>
+    </nav>
+
+    <section className="pt-10 overflow-hidden bg-gray-50 md:pt-0 sm:pt-16 2xl:pt-16">
+    <section class="py-10 bg-gray-50 sm:py-16 lg:py-20">
+    <div class="max-w-6xl px-5 mx-auto sm:px-6 lg:px-0">
+        <div class="grid items-center grid-cols-1 lg:items-stretch md:grid-cols-2 gap-y-8 gap-x-0 xl:gap-x-0">
+            <div class="relative">
+                <div class="aspect-w-4 aspect-h-3">
+                    <h2 className="text-lg font-bold leading-tight text-black sm:text-4xl lg:text-3xl">Sobre o curso Administração </h2>
+                </div>
+            </div>
+
+            <div class="flex flex-col justify-between md:py-0 max-w-7xl">
+                <blockquote>
+						<p className="md:max-w-xl mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">Administração é a ciência que se dedica ao estudo e gestão de organizações, sejam elas públicas ou privadas. Seu objetivo é promover a eficiência na gestão dos recursos, buscando atender às necessidades e demandas da sociedade e do mercado de forma eficaz.</p>
+
+						<p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">O curso de administração é uma graduação que prepara os estudantes para atuarem como gestores em empresas públicas ou privadas. O currículo inclui disciplinas como contabilidade, finanças, marketing, gestão de pessoas, empreendedorismo, estratégia e tomada de decisões. Além disso, muitas instituições oferecem atividades extracurriculares, como projetos de consultoria empresarial, estágios e intercâmbios internacionais, para complementar a formação dos alunos.</p>
+
+						<p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">O curso pode ser oferecido tanto na modalidade presencial quanto a distância, com duração média de quatro anos. Durante a graduação, os estudantes também têm a oportunidade de realizar pesquisas e desenvolver projetos práticos que contribuem para a formação de uma visão crítica e analítica sobre o mercado e o ambiente empresarial.</p>
+                
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">Ao final da graduação, o estudante estará apto a atuar em diferentes áreas da empresa, como financeira, de recursos humanos, de marketing, de produção, entre outras, podendo exercer funções de liderança, planejamento estratégico e gestão de equipes.</p>
+
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">O principal objetivo da Administração é promover a eficiência na gestão de organizações, visando atender às necessidades e demandas da sociedade e do mercado. Para isso, a área utiliza conhecimentos, técnicos e ferramentas específicas para gerir recursos e processos.</p>
+
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">De acordo com o Censo da Educação Superior 2021, que foi divulgado no ano de 2023, o curso de Administração é protagonista no ranking de formações com o maior número de ingressantes, matriculados e concluintes.</p>
+
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">A pesquisa do Instituto Nacional de Estudos e Pesquisas Educacionais Anísio Teixeira (Inep) estima que 620.966 estudantes se matricularam no curso de Administração. Isso significa o terceiro maior indicador, atrás de Pedagogia (789.254) e Direito (702.485).</p>
+
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">De acordo com o número de alunos ingressantes, a faculdade de Administração ocupa a segunda colocação com 284.096 estudantes em 2021. A primeira colocação é do curso de Pedagogia com 284.306 ingressos.</p> 
+
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">Por fim, a terceira colocação nos índices de alunos que concluíram o curso pertence ao curso de Administração. 85.427 estudantes se graduaram como bacharéis em Administração. A lista do Inep aponta Pedagogia (165.439) e Direito (115.239), respectivamente, com os maiores índices de conclusão.</p>
+
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">Na Rede Federal e sobre os cursos EaD, Administração foi o quarto curso com maior número de matrículas em 2021: 6.700. Logo em seguida, o curso de Administração Pública aparece no ranking com 6.681. Por outro lado, abordando a Rede Privada, o curso de Administração é o segundo (Atrás de Pedagogia) com o maior número de matrículas em cursos EaD em 2021: 341.139 estudantes se matricularam, segundo o último Censo.</p>
+
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">Veja também: Se você tem interesse de cursar Administração, pode encontrar bolsas de estudos que deixam as mensalidades mais baratas. Uma das boas opções de graduação nessa área com bolsas de estudo é o curso de Administração da Unirios.</p>
+                </blockquote>
+            
+                <blockquote>
+                  <h2 className="text-lg font-bold leading-tight text-black sm:text-4xl lg:text-3xl mt-6">Como é a faculdade de Administração?</h2>
+						<p className="md:max-w-xl mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">A primeira metade do bacharelado em Administração traz matérias mais básicas, incluindo matemática, direito, sociologia, informática, contabilidade e economia.</p>
+
+						<p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">A partir do terceiro ano, o aluno entra em contato com disciplinas específicas, como marketing, logística e recursos humanos. Há também atividades práticas envolvendo a análise e resolução de cases.</p>
+
+						<p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">O curso apresenta conceitos diversificados de múltiplos segmentos da Administração, como gestão financeira, marketing e recursos humanos. Nesse sentido, o profissional poderá se especializar em determinada área de atuação. As possibilidades são variadas e englobam setores como:</p>
+                
+
+  
+
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">Por fim, a terceira colocação nos índices de alunos que concluíram o curso pertence ao curso de Administração. 85.427 estudantes se graduaram como bacharéis em Administração. A lista do Inep aponta Pedagogia (165.439) e Direito (115.239), respectivamente, com os maiores índices de conclusão.</p>
+
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">Na Rede Federal e sobre os cursos EaD, Administração foi o quarto curso com maior número de matrículas em 2021: 6.700. Logo em seguida, o curso de Administração Pública aparece no ranking com 6.681. Por outro lado, abordando a Rede Privada, o curso de Administração é o segundo (Atrás de Pedagogia) com o maior número de matrículas em cursos EaD em 2021: 341.139 estudantes se matricularam, segundo o último Censo.</p>
+
+            <p className="max-w- mt-3 text-lg leading-relaxed text-gray-600 md:mt-8">Veja também: Se você tem interesse de cursar Administração, pode encontrar bolsas de estudos que deixam as mensalidades mais baratas. Uma das boas opções de graduação nessa área com bolsas de estudo é o curso de Administração da Unirios.</p>
+                </blockquote>
+
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+
+		</section>
+</section>
+
+
+
+
+
+  </section>
+  
+  
+    )
+  }
+  
+  export default Administracao
